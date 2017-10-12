aebaf52b49b2c3c0ebd5c06fd58fb7fa3ecd2b93
Roman Havdo <roman.havdo@gmail.com>
2017-10-12 13:39:12 +0300

    Add SoftHome52

---
+++ HomeWork/HW5/SoftHome52.java
@@ -0,0 +1,34 @@
+package SoftHW5;
+
+import java.util.Scanner;
+
+public class SoftHome52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean cheack=false;
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            if (array[i] > 0) {
+                    sum += array[i];
+                    System.out.println("sum= " + sum);
+            }
+
+
+            else {
+                int product = 1;
+                for (int j = 5; j < array.length; j++) {
+                        product *= array[j];
+                }
+                System.out.println("Product= "+product);
+                break;
+            }
+        }
+    }
+}
