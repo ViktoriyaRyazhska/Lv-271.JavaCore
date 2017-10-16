7d26098257d105f67046545b3a68e02beaaba2c7
LyubomyrKovch <32360164+LyubomyrKovch@users.noreply.github.com>
2017-10-16 11:07:41 +0300

    Update test2.java
    wrote a cycle

--- Homework_5/test2.java
+++ Homework_5/test2.java
@@ -6,20 +6,26 @@ public class test2 {
 		int numbers [] = {5, 15, 10, 27, -7, -9, 20, 0, 97, -4};
 		int sum =0;
 		int product =1;
-			if((numbers [0]>0) && (numbers [1]>0) && (numbers [2]>0) && (numbers [3]>0) && (numbers [4]>0)){
-				for(int i=0; i<5; i++) {
+		boolean flag = true;
+		for(int i=0; i<5; i++) {
+			if(numbers[i]<0) {
+				flag = false;
+			}
+		}
+		if(flag == true) {
+			for(int i=0; i<5; i++) {
+				if(numbers[i]>0) {
 					sum += numbers[i];
-					continue;
 				}
-				System.out.println("sum of first 5 elements: " + sum);
 			}
-			else {
-				for(int i=numbers.length - 5; i<numbers.length; i++) {
-					product *=numbers[i];
-					continue;
-				}
-				System.out.println("product of last 5 element: " + product);
+			System.out.println("sum of first 5 elements: " + sum);
+		}
+		if(flag == false) {
+			for(int i=numbers.length - 5; i<numbers.length; i++) {
+				product *=numbers[i];
 			}
+			System.out.println("product of last 5 element: " + product);
+		}
 			
 	}
 
