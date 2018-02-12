882b749a89204b410e39555087aefc3582c8acad
RomanHavdo <32357324+RomanHavdo@users.noreply.github.com>
2018-02-12 23:41:38 +0200

    Better

--- HomeWork/HW5/SoftHome53.java
+++ HomeWork/HW5/SoftHome53.java
@@ -2,39 +2,27 @@ package HW5;
 
 import java.util.Scanner;
 
-public class SoftHome53 {
+public class Home02 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] array=new int[5];
-
-        for(int i=0;i<array.length;i++){
-            array[i] = sc.nextInt();
-        }
-
-        int count=0;
-        int pos=0;
-        for(int i=0;i<array.length;i++){
-            if(array[i]>0){
-                count++;
+        Scanner sc=new Scanner(System.in);
+       int arr[]=new int[5];
+       int counter=0;
+       int min=arr[0];
+       int imin=0;
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Plz input number");
+            arr[i]=sc.nextInt();
+            if(arr[i]>0){
+                counter++;
             }
-            if(count==2){
-                pos=i;
-                break;
+            if(counter==2){
+                System.out.println("Second positive number is "+arr[i]+" and his index is "+i);
+            }
+            if(min>arr[i]){
+                min=arr[i];
+                imin=i;
             }
         }
-
-            int min = array[0];
-            int imin = 0;
-            int i = 0;
-            while (i < array.length) {
-                if (array[i] < min) {
-                    min = array[i];
-                    imin = i;
-                }
-                i++;
-            }
-            System.out.println("Position of second positive number is "+(pos+1));
-            System.out.print("Minimum = " + min);
-            System.out.println(" is in " + (imin + 1) + " place");
-        }
+        System.out.println("Min="+min+"and his pisition is "+imin);
+    }
 }
