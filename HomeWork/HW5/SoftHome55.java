d3a0daef69ff03fcba8b4c75e91da66e2e81f512
RomanHavdo <32357324+RomanHavdo@users.noreply.github.com>
2018-02-13 00:23:03 +0200

    Update SoftHome55.java

--- HomeWork/HW5/SoftHome55.java
+++ HomeWork/HW5/SoftHome55.java
@@ -2,52 +2,45 @@ package HW5;
 
 import java.util.Scanner;
 
-public class SoftHome55 {
+public class Home02 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-
-        Car cr = new Car("Mazda",1997,"1000cc");
-        Car cr1 = new Car("Honda",1998,"900cc");
-        Car cr2= new Car("BMW",1999,"800cc");
-        Car cr3 = new Car("Chevrolet",2000,"2500cc");
-
-
-        System.out.println("Input year of car");
         int inputYear=sc.nextInt();
+        Car car1=new Car(2015,"250km");
+        Car car2=new Car(2013,"150km");
+        Car car3=new Car(2012,"100km");
+        Car car4=new Car(2010,"300km");
 
-        if(inputYear==cr.getYear()){
-            System.out.println(cr);
+        if(inputYear==car1.getYearOfProduction()){
+            System.out.println(car1);
         }
-        if(inputYear==cr1.getYear()){
-            System.out.println(cr1);
+        if(inputYear==car2.getYearOfProduction()){
+            System.out.println(car2);
         }
-        if(inputYear==cr2.getYear()){
-            System.out.println(cr2);
+        if(inputYear==car3.getYearOfProduction()){
+            System.out.println(car3);
         }
-        if(inputYear==cr3.getYear()){
-            System.out.println(cr3);
+        if(inputYear==car4.getYearOfProduction()){
+            System.out.println(car4);
         }
+        int years[]=new int[4];
 
-        int[] years=new int[4];
+        years[0]=car1.getYearOfProduction();
+        years[1]=car2.getYearOfProduction();
+        years[2]=car3.getYearOfProduction();
+        years[3]=car4.getYearOfProduction();
 
-        years[0]=cr.getYear();
-        years[1]=cr1.getYear();
-        years[2]=cr2.getYear();
-        years[3]=cr3.getYear();
-
-        int tmp;
-        for (int i = 0; i < years.length - 1; i++) {
-            for (int j = i + 1; j < years.length; j++) {
-                if (years[i] < years[j]) {
-                    tmp = years[i];
-                    years[i] = years[j];
-                    years[j] = tmp;
-                }
+        int temp;
+        for(int i=0;i<(years.length-1);i++){
+                if(years[i]<years[i+1]){
+                    temp=years[i];
+                    years[i]=years[i+1];
+                    years[i+1]=temp;
             }
+
         }
         for (int i = 0; i < years.length; i++) {
             System.out.println(years[i]);
         }
-
     }
 }
